63223e1aad2bf1acb65aa6c4fb89c17c961e635f
will sherman <willsherman771@gmail.com>
2023-07-09 20:03:06 -0400

    city input done. ready for 5 day.

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const apiKey =  '428a95b212df9fb861cccf71f50f8d11';
+const apiUrl =  'https://api.openweathermap.org/data/2.5/weather?&q=';
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button')
+
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = data.main.temp;
+    document.querySelector('.humidity').innerHTML = data.main.humidity;
+    document.querySelector('.wind').innerHTML = Math.round(data.wind.speed) + 'MPH';
+}
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+})
+checkWeather(city);
+
+
